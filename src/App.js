a7d91af8e62488b11c146f61b36729fc54262615
phuongtv <phuongtv@vissoft.vn>
2022-12-16 09:01:26 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { commerce } from './lib/commerce';
+import { Products, Navbar } from './components';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Products products={products}/>
     </div>
   );
 }
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Grid } from '@material-ui/core';
+import Product from "./Product/Product";
+import useStyles from './styles'
+
+// const products = [
+//     { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$123', image: 'https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg' },
+//     { id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$124', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202203300232' },
+//     { id: 3, name: 'Phone', description: 'Mobile Galaxy.', price: '$125', image: 'https://t-mobile.scene7.com/is/image/Tmusprod/2683701-FG_Samsung_Galaxy-Z-Fold4_nologo-4?wid=750&hei=750&fmt=png-alpha' }
+// ]
+
+const Products = ({ products }) => {
+    const classes = useStyles();
+    return (
+        <main className={classes.content}>
+            <div className={classes.toolbar}></div>
+            <Grid container justifyContent="center" spacing={4}>
+                {products.map((product) => (
+                    <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product} />
+                    </Grid>
+                ))}
+            </Grid>
+        </main>
+    )
+}
+
+export default Products
